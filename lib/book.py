c392c96bab7517ec1214747d7a10429b0d0cc4de
student.moringaschool.com <guy.nkurunziza@student.moringaschool.com>
2025-05-17 15:46:59 +0200

    add

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,18 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title="Title", page_count=0):
+        self.title = title
+        self.page_count = page_count  # Utilise le setter ici
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, count):
+        if isinstance(count, int):
+            self._page_count = count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self._condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    @property
+    def condition(self):
+        return self._condition
+
+    @condition.setter
+    def condition(self, value):
+        self._condition = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
